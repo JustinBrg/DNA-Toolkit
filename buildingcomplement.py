d8f9641f2148ecbd4ed7559dcafb2a633fff54a3
JustinBrg <46937573+JustinBrg@users.noreply.github.com>
2020-07-25 01:20:01 +0200

    Update buildingcomplement.py

--- buildingcomplement.py
+++ buildingcomplement.py
@@ -15,5 +15,6 @@ for nuc in dna_string:
         complement.append('G')
 
 string = "".join([n.strip() for n in complement])
-print(" DNA            : 5' -> " + dna_string + " 3'")
-print(" DNA COMPLEMENT : 5' -> " + string[::-1] + " 3'")
+print(" DNA                : 5' -> " + dna_string + " 3'")
+print(f"                            { ''.join(['|' for c in range(len(dna_string))])} ")
+print(" Reverse COMPLEMENT : 5' -> " + string[::-1] + " 3'")
