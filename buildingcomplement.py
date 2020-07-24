49219bb58b469842895d46efb6711e63c428c810
JustinBrg <46937573+JustinBrg@users.noreply.github.com>
2020-07-25 01:07:20 +0200

    Reverse Complement of DNA
    Building the reverse Complement of the DNA reading in a FASTA file

---
+++ buildingcomplement.py
@@ -0,0 +1,19 @@
+import sys
+
+dna_string = open(sys.argv[1])
+dna_string = dna_string.read()
+
+complement = []
+for nuc in dna_string:
+    if nuc == 'A':
+        complement.append('T')
+    elif nuc == 'G':
+        complement.append('C')
+    elif nuc == 'T':
+        complement.append('A')
+    else:
+        complement.append('G')
+
+string = "".join([n.strip() for n in complement])
+print(" DNA            : 5' -> " + dna_string + " 3'")
+print(" DNA COMPLEMENT : 5' -> " + string[::-1] + " 3'")
